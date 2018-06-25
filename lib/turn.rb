eb075edba556e25a0b2a35da64a3e9b2b701d0fd
maddymthompson <madeline.thompson.17@tfacorps.org>
2018-06-25 23:16:20 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -33,7 +33,7 @@ end
 
 
 def valid_move?(board, index)
-  index.between?(0,8) && !position_taken?(board, index)
+  index.is.a?(Integer) && index.between?(0,8) && !position_taken?(board, index)
 end
 
 
