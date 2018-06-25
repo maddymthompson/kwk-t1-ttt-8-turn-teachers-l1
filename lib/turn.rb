fdc118611d5f2130e1c7f5581aaac3283383905d
maddymthompson <madeline.thompson.17@tfacorps.org>
2018-06-25 23:14:17 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,12 +1,12 @@
-board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
-
-puts "Welcome to Tic Tac Toe!"
-display_board(board)
-turn(board)
-
-def turn(board)
-  puts "Please enter 1-9:"
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
+
+# code your input_to_index and move method here!
 def input_to_index(input)
   input.to_i-1
 end
